04f6b3e4243a9ab109f17eb68ea662d687f819cc
Lucas Montuori-Sorrentino <com@montuori-sorrentino.com>
2026-01-11 00:29:18 -0500

    vits-web hinzugefügt (schnellere version)

--- assets/vits-web/example/index.ts
+++ assets/vits-web/example/index.ts
@@ -1,16 +1,17 @@
 import * as tts from '../src';
 import Worker from './worker.ts?worker';
+import Worker2 from './worker2.ts?worker';
 
 // required for e2e
 Object.assign(window, { tts });
 
 document.querySelector('#app')!.innerHTML = `
 <button id="btn" type="button">Predict</button>
+<button id="btn2" type="button">Predict with Session Reuse</button>
 `
 
-const worker = new Worker();
-
 document.getElementById('btn')?.addEventListener('click', async () => {
+  const worker = new Worker();
 
   worker.postMessage({
     type: 'init',
@@ -24,5 +25,24 @@ document.getElementById('btn')?.addEventListener('click', async () => {
     const audio = new Audio();
     audio.src = URL.createObjectURL(event.data.audio);
     audio.play();
+    worker.terminate();
   });
+});
+
+const mainWorker = new Worker2();
+
+document.getElementById('btn2')?.addEventListener('click', async () => {
+  mainWorker.postMessage({
+    type: 'init',
+    text: "As the waves crashed against the shore, they carried tales of distant lands and adventures untold.",
+    voiceId: 'en_US-hfc_female-medium',
+  });
+
+  mainWorker.addEventListener('message', (event: MessageEvent<{ type: 'result', audio: Blob }>) => {
+    if (event.data.type != 'result') return;
+
+    const audio = new Audio();
+    audio.src = URL.createObjectURL(event.data.audio);
+    audio.play();
+  }, { once: true });
 });
--- assets/vits-web/src/inference.spec.ts
+++ assets/vits-web/src/inference.spec.ts
@@ -6,75 +6,76 @@ test.describe.configure({ mode: 'serial' });
 let page: Page;
 
 test.describe('The inference methods', () => {
-	test.beforeAll(async ({ browser }) => {
-		page = await browser.newPage();
-		await page.goto('http://localhost:5173/');
-	});
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto('http://localhost:5173/');
+  });
 
-	test.afterEach(async () => {
-		await page.evaluate(async () => {
-			// @ts-ignore
-			await (await navigator.storage.getDirectory()).remove({ recursive: true });
-		});
-	});
+  test.afterEach(async () => {
+    await page.evaluate(async () => {
+      // @ts-ignore
+      await (await navigator.storage.getDirectory()).remove({ recursive: true });
+    });
+  })
 
-	test('should be able to generate a voice and download models', async () => {
-		let stored = await page.evaluate(async () => {
-			return await tts.stored();
-		});
-		// make sure opfs is empty
-		expect(stored.length).toBe(0);
+  test('should be able to generate a voice and download models', async () => {
 
-		// load model from huggingface
+    let stored = await page.evaluate(async () => {
+      return await tts.stored();
+    });
+    // make sure opfs is empty
+    expect(stored.length).toBe(0);
 
-		const result = await page.evaluate(async () => {
-			const calls: tts.Progress[] = [];
+    // load model from huggingface
 
-			const fn: tts.ProgressCallback = (progress) => {
-				calls.push(progress);
-			};
+    const result = await page.evaluate(async () => {
+      const calls: tts.Progress[] = [];
 
-			const audio = await tts.predict({ text: 'Hello World', voiceId: 'en_US-danny-low' }, fn);
-			const arrayBuffer = await audio.arrayBuffer();
-			const { size, type } = audio;
+      const fn: tts.ProgressCallback = (progress) => {
+        calls.push(progress);
+      }
 
-			return {
-				calls,
-				size,
-				type,
-				byteLength: arrayBuffer.byteLength,
-			};
-		});
+      const audio = await tts.predict({ text: 'Hello World', voiceId: 'en_US-danny-low' }, fn);
+      const arrayBuffer = await audio.arrayBuffer();
+      const { size, type } = audio;
 
-		// check progress
-		expect(result.calls.length).toBeGreaterThan(10);
-		expect(result.calls[10].url).toMatch('en_US-danny-low');
-		expect(typeof result.calls[10].total == 'number').toBe(true);
-		expect(typeof result.calls[10].loaded == 'number').toBe(true);
+      return {
+        calls,
+        size,
+        type,
+        byteLength: arrayBuffer.byteLength,
+      }
+    });
 
-		expect(result.byteLength).toBeGreaterThan(1e3);
-		expect(result.size).toBeGreaterThan(1e3);
-		expect(result.type).toBe('audio/x-wav');
+    // check progress
+    expect(result.calls.length).toBeGreaterThan(10);
+    expect(result.calls[10].url).toMatch('en_US-danny-low');
+    expect(typeof result.calls[10].total == 'number').toBe(true);
+    expect(typeof result.calls[10].loaded == 'number').toBe(true);
 
-		stored = await page.evaluate(async () => {
-			return await tts.stored();
-		});
-		// make sure opfs is empty
-		expect(stored.length).toBe(1);
+    expect(result.byteLength).toBeGreaterThan(1e3);
+    expect(result.size).toBeGreaterThan(1e3);
+    expect(result.type).toBe('audio/x-wav');
 
-		// load model from memory
-		// use the same model again
-		const calls = await page.evaluate(async () => {
-			const calls: tts.Progress[] = [];
+    stored = await page.evaluate(async () => {
+      return await tts.stored();
+    });
+    // make sure opfs is empty
+    expect(stored.length).toBe(1);
 
-			const fn: tts.ProgressCallback = (progress) => {
-				calls.push(progress);
-			};
+    // load model from memory
+    // use the same model again
+    const calls = await page.evaluate(async () => {
+      const calls: tts.Progress[] = [];
 
-			await tts.predict({ text: 'Hello World', voiceId: 'en_US-danny-low' }, fn);
-			return calls;
-		});
+      const fn: tts.ProgressCallback = (progress) => {
+        calls.push(progress);
+      }
 
-		expect(calls.length).toBe(0);
-	});
+      await tts.predict({ text: 'Hello World', voiceId: 'en_US-danny-low' }, fn);
+      return calls
+    });
+
+    expect(calls.length).toBe(0);
+  });
 });
