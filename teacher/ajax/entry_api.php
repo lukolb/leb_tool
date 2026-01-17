e8cb97a3c2a2f139a169faf0d54ba26ffecb4c96
lukolb <com@lucas-kolb.de>
2026-01-17 23:32:37 +0100

    Increase AI response length further

--- teacher/ajax/entry_api.php
+++ teacher/ajax/entry_api.php
@@ -231,7 +231,7 @@ function ai_chat_completion(array $messages, array $aiCfg): string {
     'model' => $aiCfg['model'],
     'messages' => $messages,
     'temperature' => 0.7,
-    'max_tokens' => 900,
+    'max_tokens' => 1500,
   ];
 
   $ch = curl_init($url);
