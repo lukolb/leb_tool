4037c1f695350ceb35f74416b5c105ae81686837
lukolb <com@lucas-kolb.de>
2026-01-20 05:40:42 +0100

    Support assets TCPDF file naming

--- lib/pdf_vendor.php
+++ lib/pdf_vendor.php
@@ -1,8 +1,16 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../assets/tcpdf/tcpdf.php';
-require_once __DIR__ . '/../assets/fpdi/src/autoload.php';
+if (is_file(__DIR__ . '/../assets/tcpdf/tcpdf.php')) {
+  require_once __DIR__ . '/../assets/tcpdf/tcpdf.php';
+} elseif (is_file(__DIR__ . '/../assets/tcpdf/Tcpdf.php')) {
+  require_once __DIR__ . '/../assets/tcpdf/Tcpdf.php';
+  if (class_exists('Com\\Tecnick\\Pdf\\Tcpdf') && !class_exists('TCPDF')) {
+    class_alias('Com\\Tecnick\\Pdf\\Tcpdf', 'TCPDF');
+  }
+}
+
+require_once __DIR__ . '/../assets/fpdi/autoload.php';
 
 if (!class_exists('TCPDF')) {
   throw new RuntimeException('TCPDF ist nicht verfügbar. Bitte /assets/tcpdf installieren.');
